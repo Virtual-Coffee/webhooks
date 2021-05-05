df801156c3b7d9ca438a93d81cfb3c3149fc82c4
Dan Ott <dan@dtott.com>
2021-05-05 12:27:29 -0400

    update slac-events

--- functions/slack-events/index.js
+++ functions/slack-events/index.js
@@ -1,8 +1,22 @@
 const handler = async function (event, context) {
+  //   {
+  //     "token": "Jhj5dZrVaK7ZwHHjRyZWjbDl",
+  //     "challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
+  //     "type": "url_verification"
+  // }
+
+  const request = JSON.parse(event.body);
+  if (request.challenge) {
+    return {
+      statusCode: 200,
+      body: request.challenge,
+      // body: JSON.stringify({ identity, user, msg: data.value }),
+    };
+  }
+
   return {
     statusCode: 200,
     body: '',
-    // body: JSON.stringify({ identity, user, msg: data.value }),
   };
 };
 
