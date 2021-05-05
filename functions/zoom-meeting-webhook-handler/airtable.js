209f35ebb9811b364fded638336dbbc509f84de8
Dan Ott <dan@dtott.com>
2021-05-05 09:06:39 -0400

    Add support for slack bot

---
+++ functions/zoom-meeting-webhook-handler/airtable.js
@@ -0,0 +1,38 @@
+// returns a roomInstance record, or undefined.
+// Will retry 5 times, pausing 1 second between tries.
+async function findRoomInstance(base, instanceId) {
+  async function tryFind() {
+    const resultArray = await base('room_instances')
+      .select({
+        // Selecting the first 1 records in Grid view:
+        maxRecords: 1,
+        view: 'Grid view',
+        filterByFormula: `instance_uuid='${instanceId}'`,
+      })
+      .firstPage();
+
+    return resultArray[0];
+  }
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  let roomInstance = await tryFind();
+  let count = 0;
+  while (count < 5 && !roomInstance) {
+    console.log('trying again');
+    count++;
+    await sleep(1000);
+    roomInstance = await tryFind();
+  }
+
+  console.log(
+    'room instance: ',
+    roomInstance.get('instance_uuid'),
+    roomInstance.get('slack_thread_timestamp')
+  );
+
+  return roomInstance;
+}
+
+module.exports = { findRoomInstance };
---
+++ functions/zoom-meeting-webhook-handler/slack.js
@@ -0,0 +1,91 @@
+require('dotenv').config();
+
+const { WebClient } = require('@slack/web-api');
+const web = new WebClient(process.env.SLACK_BOT_TOKEN);
+
+// timestamp: if we have a timestamp, that means we've ended the meeting and are trying to update the message
+// otherwise, post a new message
+
+async function updateMeetingStatus(timestamp) {
+  const message = {
+    channel: process.env.COWORKING_SLACK_CHANNEL_ID,
+    text: timestamp
+      ? 'The current Co-Working Session has ended.'
+      : 'A new Co-Working Session has Started!',
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: timestamp
+            ? 'The current Co-Working Session has ended.'
+            : 'A new Co-Working Session has Started!',
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: timestamp ? 'Start a New Session' : 'Join Now',
+            emoji: true,
+          },
+          value: 'join_meeting',
+          url:
+            'https://us02web.zoom.us/j/81323022832?pwd=Rm1PcG13RTBrMlMzQi8yaXVlc3hmdz09',
+          action_id: 'button-action',
+          style: 'primary',
+          confirm: {
+            title: {
+              type: 'plain_text',
+              text: 'Heads up!',
+            },
+            text: {
+              type: 'mrkdwn',
+              text:
+                "This is a Zoom link - following it will most likely open Zoom and add you to our Co-Working Room. \n\n Just want to make sure we're all on the same page 😃",
+            },
+            confirm: {
+              type: 'plain_text',
+              text: "Let's go!",
+            },
+            deny: {
+              type: 'plain_text',
+              text: "Stop, I've changed my mind!",
+            },
+          },
+        },
+      },
+    ],
+  };
+
+  console.log({ timestamp });
+
+  const result = timestamp
+    ? await web.chat.update({ ...message, ts: timestamp })
+    : await web.chat.postMessage(message);
+
+  console.log(
+    `Successfully send message ${result.ts} in conversation ${process.env.COWORKING_SLACK_CHANNEL_ID}`
+  );
+
+  return result;
+}
+
+async function updateMeetingAttendence(thread_ts, zoomRequest) {
+  const username = zoomRequest.payload.object.participant.user_name;
+  const result = await web.chat.postMessage({
+    thread_ts,
+    text:
+      zoomRequest.event === 'meeting.participant_joined'
+        ? `${username} has joined!`
+        : `${username} has left. We'll miss you!`,
+    channel: process.env.COWORKING_SLACK_CHANNEL_ID,
+  });
+
+  console.log(
+    `Successfully send message ${result.ts} in conversation ${process.env.COWORKING_SLACK_CHANNEL_ID}`
+  );
+
+  return result;
+}
+
+module.exports = { updateMeetingStatus, updateMeetingAttendence };
