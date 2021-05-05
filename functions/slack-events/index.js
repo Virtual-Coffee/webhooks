4252ed5068aaaa3dfd52dd3469823c887fd845c3
Dan Ott <dan@dtott.com>
2021-05-05 13:52:03 -0400

    wrap in try

--- functions/slack-events/index.js
+++ functions/slack-events/index.js
@@ -4,13 +4,19 @@ const handler = async function (event, context) {
   //     "challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
   //     "type": "url_verification"
   // }
-
-  const request = JSON.parse(event.body);
-  if (request.challenge) {
+  try {
+    const request = JSON.parse(event.body);
+    if (request.challenge) {
+      return {
+        statusCode: 200,
+        body: request.challenge,
+        // body: JSON.stringify({ identity, user, msg: data.value }),
+      };
+    }
+  } catch (error) {
     return {
       statusCode: 200,
-      body: request.challenge,
-      // body: JSON.stringify({ identity, user, msg: data.value }),
+      body: '',
     };
   }
 
