e97907b1ec9259d254dffeabafb472057ba4c2a4
Dan Ott <dan@dtott.com>
2021-05-06 10:05:00 -0400

    add first pass at greetings

--- functions/slack-events/index.js
+++ functions/slack-events/index.js
@@ -1,4 +1,51 @@
+require('dotenv').config();
+const crypto = require('crypto');
+
+const { WebClient } = require('@slack/web-api');
+// const web = new WebClient(process.env.SLACK_BOT_TOKEN);
+const web = new WebClient(
+  'xoxb-2058222876048-2047060586769-vbavfhv9iv0pYfyW93rfhnfX'
+);
+
+function verify(event) {
+  const slackSignature = event.headers['x-slack-signature'];
+  const timestamp = event.headers['x-slack-request-timestamp'];
+  // convert current time from milliseconds to seconds
+  const time = Math.floor(new Date().getTime() / 1000);
+  if (Math.abs(time - timestamp) > 300) {
+    return {
+      valid: false,
+      reason: 'Ignore this request.',
+    };
+  }
+
+  const verificationString = `v0:${timestamp}:${event.body}`;
+  const mySignature =
+    'v0=' +
+    crypto
+      .createHmac('sha256', process.env.SLACK_SIGNING_SECRET)
+      .update(verificationString, 'utf8')
+      .digest('hex');
+
+  if (
+    crypto.timingSafeEqual(
+      Buffer.from(mySignature, 'utf8'),
+      Buffer.from(slackSignature, 'utf8')
+    )
+  ) {
+    return {
+      valid: true,
+    };
+  } else {
+    return {
+      valid: false,
+      reason: 'Verification Failed.',
+    };
+  }
+}
+
 const handler = async function (event, context) {
+  // https://vc-webhooks-335424.netlify.live/.netlify/functions/slack-events
   //   {
   //     "token": "Jhj5dZrVaK7ZwHHjRyZWjbDl",
   //     "challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
@@ -6,24 +53,60 @@ const handler = async function (event, context) {
   // }
   try {
     const request = JSON.parse(event.body);
-    if (request.challenge) {
-      return {
-        statusCode: 200,
-        body: request.challenge,
-        // body: JSON.stringify({ identity, user, msg: data.value }),
-      };
+    console.log(request);
+    switch (request.type) {
+      case 'url_verification':
+        if (request.challenge) {
+          return {
+            statusCode: 200,
+            body: request.challenge,
+            // body: JSON.stringify({ identity, user, msg: data.value }),
+          };
+        }
+        break;
+      case 'event_callback':
+        const isValid = verify(event);
+
+        if (!isValid.valid) {
+          return {
+            statusCode: 400,
+            body: isValid.reason,
+          };
+        }
+        // v0
+
+        const result = await web.chat.postMessage({
+          text: 'Hello!',
+          channel: request.event.user,
+        });
+
+        console.log(
+          `Successfully send message ${result.ts} to user ${request.event.user}`
+        );
+
+        return {
+          statusCode: 200,
+          body: '',
+        };
+
+      default:
+        return {
+          statusCode: 200,
+          body: '',
+        };
     }
   } catch (error) {
+    console.log(error);
     return {
       statusCode: 200,
       body: '',
     };
   }
 
-  return {
-    statusCode: 200,
-    body: '',
-  };
+  // return {
+  //   statusCode: 200,
+  //   body: '',
+  // };
 };
 
 module.exports = { handler };
---
+++ functions/slack-send-message-background/messages.js
@@ -0,0 +1,92 @@
+exports.welcome = ({ username }) => ({
+  blocks: [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `👋 Hey @${username}, welcome to Virtual Coffee--fondly referred to as VC around this space.`,
+      },
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text:
+          "❤️ Before doing anything else, please first take a moment to read our <https://virtualcoffee.io/code-of-conduct|Code of Conduct>. Our Code of Conduct is in effect at any Virtual Coffee function, including direct messages. If you have experienced or witnessed violations to Virtual Coffee's Code of Conduct, please use our <https://virtualcoffee.io/report-coc-violation/|Code of Conduct Violation Form> to let us know.",
+      },
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'header',
+      text: {
+        type: 'plain_text',
+        text: 'Now for the fun part!',
+        emoji: true,
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text:
+          'We have a lot going on here, but here are some places you might want to start:',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text:
+          '✅ Head over to #welcome and introduce yourself to the rest of the group!',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text:
+          '🎯 Check out #monthly-challenge to see what the community is working on together right now.',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text:
+          '📣 The #announcements channel has the most recent news on events and initiatives happening in the community.',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text:
+          "💻 Our #co-working-room is a zoom room that's open all day, every day for members to quietly work, pair on solving problems, or just say hello.",
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text:
+          '❓ #help-and-pairing is the space for asking questions about any and all tech related topics. But if you have a general question, we have a really welcoming community, so feel free to throw it in the channel that looks best.',
+      },
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text:
+          "❤️ And remember, you can always message one of our community maintainers, @rhawrot, @thesaramccombs, or @tkshillingford for any help and support you may need. \n\n *We're happy to have you here!*",
+      },
+    },
+  ],
+});
