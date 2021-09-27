e4c23787dd56b8b08f825307915c5fb5d2e0b8ca
Dan Ott <dan@dtott.com>
2021-09-27 14:39:38 -0400

    pull room data from airtable on build

---
+++ scripts/build.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const Airtable = require('airtable');
+const base = new Airtable().base(process.env.AIRTABLE_COWORKING_BASE);
+var fs = require('fs');
+const path = require('path');
+
+async function main() {
+  console.log('Building rooms');
+  const results = await base('rooms').select().all();
+
+  const rooms = results.map((record) => record.fields);
+
+  fs.writeFileSync(
+    path.resolve(__dirname, '..', 'data', 'rooms.json'),
+    JSON.stringify(rooms, null, 2)
+  );
+
+  console.log(`Done building ${rooms.length} rooms`);
+}
+
+main();
